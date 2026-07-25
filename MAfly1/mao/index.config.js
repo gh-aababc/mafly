5742aca21e88dd3b0d2974c03c5a3fe65f674737
mhmdxahmd <mhmdahmd2006me@gmail.com>
2026-07-25 16:47:37 +0800

    Update index.config.js

--- MAfly1/mao/index.config.js
+++ MAfly1/mao/index.config.js
@@ -84,6 +84,8 @@ var index_config_default = {
       address: "https://tw.kl.maflya.com"},
       {name: "📡抖音直播源-[实时抓取]]",
       address: "https://dy.kl.maflya.com"},
+      {name: "🍟YouTube直播-[实时抓取]]",
+      address: "https://ybkl.maflya.com"},
     {name: "👖金牌影视",
       address: "https://banyeomni.780420.xyz:17777/api/tvbox/source/2070335666620731392?token=banye666"},
     {name: "👖瓜子影视",
